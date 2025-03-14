b74c31a939cd9ac39a54205ce6f8a30a7276c34d
isaacvay <isaacyavs@gmail.com>
2025-03-14 10:58:57 +0200

    Update page.tsx

--- app/dashboard/liste/[id]/page.tsx
+++ app/dashboard/liste/[id]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '@/firebase/firebase'; // Ajustez le chemin selon votre projet
+import React, { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase/firebase"; // Ajustez le chemin selon votre projet
+import QRCode from "react-qr-code";
 
 interface Agent {
   nom: string;
@@ -47,6 +48,9 @@ export default function AgentDetailPage() {
   if (error) return <div>{error}</div>;
   if (!agent) return <div>Aucun agent trouvé</div>;
 
+  // L'URL intégrant le matricule, qui sera encodée dans le QR code
+  const qrCodeValue = `https://kccverify.vercel.app/pages/verification?matricule=${agent.matricule}`;
+
   return (
     <div className="flex justify-center items-start min-h-screen bg-gray-100 uppercase">
       <div className="bg-white text-gray-900 p-4 rounded-lg shadow-lg h-[400px] w-[650px] border border-gray-400">
@@ -62,7 +66,9 @@ export default function AgentDetailPage() {
             />
           </div>
           <div className="text-center">
-            <h2 className="text-red-700 font-bold text-base uppercase">BON DE FARINE</h2>
+            <h2 className="text-red-700 font-bold text-base uppercase">
+              BON DE FARINE
+            </h2>
             <p className="text-base font-semibold">25 KG</p>
           </div>
           {/* Affichage de la photo depuis Cloudinary */}
@@ -91,7 +97,9 @@ export default function AgentDetailPage() {
           <div className="grid grid-cols-2 border-b border-gray-500">
             <div>
               <div className="grid grid-cols-2 border-b border-gray-500">
-                <p className="border-r border-gray-500 p-1 font-bold text-base">Matricule</p>
+                <p className="border-r border-gray-500 p-1 font-bold text-base">
+                  Matricule
+                </p>
                 <p className="p-1 text-base font-semibold">{agent.matricule}</p>
               </div>
               <div className="grid grid-cols-2 border-b border-gray-500">
@@ -111,13 +119,14 @@ export default function AgentDetailPage() {
 
           <div className="border-b border-gray-500">
             <p className="p-1 font-bold text-base">
-              Point de distribution : <span className="font-semibold">{agent.pointDistribution}</span>
+              Point de distribution :{" "}
+              <span className="font-semibold">{agent.pointDistribution}</span>
             </p>
           </div>
         </div>
 
         {/* Pied de page */}
-        <div className="flex justify-center items-center p-6 border-x border-b border-gray-500 text-gray-700">
+        <div className="flex justify-between  items-center p-3 border-x border-b border-gray-500 text-gray-700">
           <img
             src="/KCCLogo.svg"
             alt="Logo KCC"
@@ -125,6 +134,12 @@ export default function AgentDetailPage() {
             width={128}
             height={40}
           />
+          {/* QR Code */}
+        <div className="flex justify-center items-center">
+          <div className="bg-white p-2 rounded shadow">
+            <QRCode value={qrCodeValue} size={50} />
+          </div>
+        </div>
         </div>
       </div>
     </div>
